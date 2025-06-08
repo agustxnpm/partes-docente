13946b84d0855ae9fa9d9975cc863ee4e30344e3
Agustin Palma <aguspalqui@hotmail.com>
2025-06-08 20:46:21 -0300

    Se muestra el historial logs en orden y el backend devuelve los mensajes correctos de logs

--- frontend/cli/src/app/licencias/LogLicencia.ts
+++ frontend/cli/src/app/licencias/LogLicencia.ts
@@ -1,6 +1,6 @@
 export interface LogLicencia {
   id: number;
-  fechaLog: string;
+  fechaLog: string; // ISO 8601 string para LocalDateTime desde el backend
   mensaje: string;
   licencia: {
     id: number;
